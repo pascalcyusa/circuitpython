d3eadeb7187fbc68bc9cfac29f57b08b24b5660e
Jean Pascal Cyusa Shyaka <shyakaadolf@gmail.com>
2024-11-22 02:40:00 -0500

    this code works except for the damn button that keeps pressing itsef

--- game.py
+++ game.py
@@ -2,8 +2,8 @@ import board
 import pwmio
 import rotaryio
 import digitalio
-from adafruit_motor import stepper
 import time
+from adafruit_motor import stepper
 
 # Define PWM pins for the stepper motor with at least 1500 Hz frequency
 motor_pins = [
@@ -14,93 +14,96 @@ motor_pins = [
 ]
 
 # Create stepper motor object
-motor = stepper.StepperMotor(
+stepper_motor = stepper.StepperMotor(
     motor_pins[0], motor_pins[1], motor_pins[2], motor_pins[3]
 )
 
 # Create rotary encoder object
 encoder = rotaryio.IncrementalEncoder(board.D6, board.D7)
 
-# DC motor setup
-dc_motor_forward = pwmio.PWMOut(board.D8, frequency=2000)
-dc_motor_backward = pwmio.PWMOut(board.D9, frequency=2000)
-
-# LED setup using PWM
-green_led = pwmio.PWMOut(board.A0, frequency=1000, duty_cycle=0)  # Green LED
-
-# Button setup using analog pin as digital GPIO
-goal_button = digitalio.DigitalInOut(board.A3)  # Goal button
-goal_button.direction = digitalio.Direction.INPUT
-goal_button.pull = digitalio.Pull.UP  # Enable internal pull-up resistor
-
 # Variables to track encoder position
 last_position = 0
 
+# Define DC motor pins
+dc_motor_pin1 = pwmio.PWMOut(board.D8, frequency=5000, duty_cycle=0)
+dc_motor_pin2 = pwmio.PWMOut(board.D9, frequency=5000, duty_cycle=0)
+
+# Define button pin
+button = digitalio.DigitalInOut(board.A3)
+button.direction = digitalio.Direction.INPUT
+
+# Define green LED pin
+green_led = digitalio.DigitalInOut(board.D10)
+green_led.direction = digitalio.Direction.OUTPUT
+
 # State machine states
 IDLE = 0
-PLAYING = 1
-GOAL = 2
+GAME_PLAY = 1
+GOAL_SCORED = 2
+
+# Initial state
 state = IDLE
+
+
+def control_dc_motor():
+    # Move DC motor clockwise and counter-clockwise
+    dc_motor_pin1.duty_cycle = 0
+    dc_motor_pin2.duty_cycle = 32767  # Half duty cycle for counter-clockwise
+    time.sleep(0.5)
+    dc_motor_pin1.duty_cycle = 32767  # Half duty cycle for clockwise
+    dc_motor_pin2.duty_cycle = 0
+    time.sleep(0.5)
+    dc_motor_pin1.duty_cycle = 0
+    dc_motor_pin2.duty_cycle = 0
+
 
 try:
     while True:
-        # State: IDLE
-        if state == IDLE:
-            green_led.duty_cycle = 0  # Turn off green LED
-            print("Game is idle. Press the goal button to start...")
+        # Read the current position of the encoder
+        position = encoder.position
 
-            # Wait for goal button press to start the game
-            if not goal_button.value:  # Button pressed (active low)
-                print("Game starting!")
-                state = PLAYING
-
-        # State: PLAYING
-        elif state == PLAYING:
-            green_led.duty_cycle = 0    # Turn off green LED
-
-            # Move DC motor left (backward)
-            dc_motor_forward.duty_cycle = 0
-            dc_motor_backward.duty_cycle = 65535  # Full speed backward
-            print("DC motor moving left...")
-            time.sleep(1)  # Wait for 1 second
-
-            # Move DC motor right (forward)
-            dc_motor_forward.duty_cycle = 65535  # Full speed forward
-            dc_motor_backward.duty_cycle = 0
-            print("DC motor moving right...")
-            time.sleep(1)  # Wait for 1 second
-
-            # Check for goal button press
-            if not goal_button.value:  # Button pressed (active low)
-                print("Goal detected!")
-                state = GOAL
-
-            # Control the stepper motor based on encoder position
-            position = encoder.position
+        # Check if the encoder position has changed
+        if position != last_position:
             if position > last_position:
-                motor.onestep(direction=stepper.FORWARD, style=stepper.SINGLE)
+                # Move stepper motor forward
+                stepper_motor.onestep(
+                    direction=stepper.FORWARD, style=stepper.SINGLE)
+                print(f"Stepper motor moved forward to position {position}")
             elif position < last_position:
-                motor.onestep(direction=stepper.BACKWARD, style=stepper.SINGLE)
+                # Move stepper motor backward
+                stepper_motor.onestep(
+                    direction=stepper.BACKWARD, style=stepper.SINGLE)
+                print(f"Stepper motor moved backward to position {position}")
+
+            # Update the last position
             last_position = position
 
-        # State: GOAL
-        elif state == GOAL:
-            green_led.duty_cycle = 65535  # Brighten green LED
-            print("Goal scored! Green LED is ON.")
+        # State machine logic
+        if state == IDLE:
+            if not button.value:  # Button pressed
+                state = GAME_PLAY
+                green_led.value = False  # Turn off green LED
+                print("Game started")
 
-            # Stop the DC motor temporarily
-            dc_motor_forward.duty_cycle = 0
-            dc_motor_backward.duty_cycle = 0
+        elif state == GAME_PLAY:
+            control_dc_motor()
+            if not button.value:  # Button pressed
+                state = GOAL_SCORED
+                green_led.value = True  # Turn on green LED
+                print("Goal scored!")
 
-            time.sleep(2)  # Keep the green LED on for 2 seconds
+        elif state == GOAL_SCORED:
+            if not button.value:  # Button pressed
+                state = IDLE
+                green_led.value = False  # Turn off green LED
+                print("Game reset")
 
-            # Transition back to PLAYING state
-            state = PLAYING
+        # Small delay to debounce the button
+        time.sleep(0.5)
 
 except KeyboardInterrupt:
-    # Release resources when script is interrupted
-    motor.release()
-    dc_motor_forward.deinit()
-    dc_motor_backward.deinit()
-    green_led.duty_cycle = 0
-    print("Game stopped.")
+    # Release the motors when the script is interrupted
+    stepper_motor.release()
+    dc_motor_pin1.duty_cycle = 0
+    dc_motor_pin2.duty_cycle = 0
+    print("Motors released.")
